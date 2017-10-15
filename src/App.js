95d7acee2bf20922b7ffceaaa4002e17b8d1cd94
Tim <tim.bathgate@gmail.com>
2017-10-16 10:04:37 +1300

    Remove dead code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 import Search from './Search'
 import Main from './Main'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
 
   render() {
     return (
